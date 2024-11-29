3abe014275bffbe36ba8dcf910f29ac37e161c6c
solji-choi <solji.choi89@gmail.com>
2024-11-29 11:24:37 +0900

    t9 g

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -1,6 +1,30 @@
 package com.ll;
 
 public class SimpleCalculator {
+    public int run(String calculation) {
+        String[] calculationBits = calculation.split(" ");
+        int num1 = Integer.parseInt(calculationBits[0]);
+        int num2 = Integer.parseInt(calculationBits[2]);
+
+        try {
+            switch (calculationBits[1]) {
+                case "+" :
+                    return plus(num1, num2);
+                case "-" :
+                    return minus(num1, num2);
+                case "*" :
+                    return multifly(num1, num2);
+                case "/" :
+                    return divide(num1, num2);
+                default:
+                    return 0;
+            }
+
+        } catch (Exception  e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public int plus(int num1, int num2) {
         return num1 + num2;
     }
